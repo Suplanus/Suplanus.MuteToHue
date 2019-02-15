2cfd25b3b03db55822a4cb048d253c0d85a34011
Suplanus <info@suplanus.de>
2019-02-15 16:58:52 +0100

    Reading mute state

---
+++ src/Suplanus.MuteToHue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Suplanus.MuteToHue
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("MuteToHue started");
+      try
+      {
+        MuteToHue muteToHue = new MuteToHue();
+        muteToHue.StartAsync();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Error: " + Environment.NewLine + ex);
+      }
+    }
+  }
+}
