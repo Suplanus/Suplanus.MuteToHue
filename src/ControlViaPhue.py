c6885ff3cfa9b8f353cd462669e5ed508cdadaa2
Suplanus <info@suplanus.de>
2019-02-16 07:49:18 +0100

    Simplify python

--- src/ControlViaPhue.py
+++ src/ControlViaPhue.py
@@ -7,13 +7,10 @@ import logging
 HUEBRIDGEIP = "192.168.178.79"
 LIGHTNAME = "OnAir" # OnAir # Buro
 
-# Init hue
-logging.basicConfig()
-
-Bridge = Bridge(HUEBRIDGEIP)
+logging.basicConfig() # lgging
+Bridge = Bridge(HUEBRIDGEIP) # Get bridge
 Bridge.connect() # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
 Bridge.get_api() # Get the bridge state (This returns the full dictionary that you can explore)
 light_names = Bridge.get_light_objects('name') # Get a dictionary with the light name as the key
 LAMP = light_names[LIGHTNAME] # Get light object
 LAMP.on = $STATE$ # True # $STATE$
-LAMP.brightness = 254
